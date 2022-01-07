b2f2a2cabe9bd72d900b6d423b145df8ed6f0656
flavie <flaviegallant@hotmail.com>
2022-01-07 18:04:33 -0500

    Ajout Pages

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import { utiliserDB } from './bd/connexion';
+import { utiliserDB } from './baseDeDonnes/connexion';
+import { ObjectID } from 'mongod';
 
 const app = express();
 app.listen(8000, () => console.log("Jecoute sur le port 8000"));
@@ -27,67 +28,67 @@ app.get('/api/pieces/:id', (requete, reponse) => {
     );
 });
 
-app.put('/api/pieces/ajouter', (requete, reponse) => {
-    const {titre, artiste, categorie} = requete.body;
+// app.put('/api/pieces/ajouter', (requete, reponse) => {
+//     const {titre, artiste, categorie} = requete.body;
 
-    if (titre !== undefined && artiste !== undefined && categorie !== undefined) {
-        utiliserDB(async (db) => {
-            await db.collection('pieces').insertOne({ 
-                titre: titre,
-                artiste: artiste,
-                categorie: categorie
-            });
+//     if (titre !== undefined && artiste !== undefined && categorie !== undefined) {
+//         utiliserDB(async (db) => {
+//             await db.collection('pieces').insertOne({ 
+//                 titre: titre,
+//                 artiste: artiste,
+//                 categorie: categorie
+//             });
             
-            reponse.status(200).send("Pièce ajoutée");
-        }, reponse).catch(
-            () => reponse.status(500).send("Erreur : la pièce n'a pas été ajoutée")
-        );        
-    }
-    else {
-        reponse.status(500).send(`Certains paramètres ne sont pas définis :
-            - titre: ${titre}
-            - artiste: ${artiste}
-            - categorie: ${categorie}`);
-    }
-});
+//             reponse.status(200).send("Pièce ajoutée");
+//         }, reponse).catch(
+//             () => reponse.status(500).send("Erreur : la pièce n'a pas été ajoutée")
+//         );        
+//     }
+//     else {
+//         reponse.status(500).send(`Certains paramètres ne sont pas définis :
+//             - titre: ${titre}
+//             - artiste: ${artiste}
+//             - categorie: ${categorie}`);
+//     }
+// });
 
-app.post('/api/pieces/modifier/:id', (requete, reponse) => {
-    const {titre, artiste, categorie} = requete.body;
-    const id = requete.params.id;
+// app.post('/api/pieces/modifier/:id', (requete, reponse) => {
+//     const {titre, artiste, categorie} = requete.body;
+//     const id = requete.params.id;
 
-    if (titre !== undefined && artiste !== undefined && categorie !== undefined) {
-        utiliserDB(async (db) => {
-            var objectId = ObjectID.createFromHexString(id);
-            await db.collection('pieces').updateOne({ _id: objectId }, {
-                '$set': {
-                    titre: titre,
-                    artiste: artiste,
-                    categorie: categorie
-                }
-            });
+//     if (titre !== undefined && artiste !== undefined && categorie !== undefined) {
+//         utiliserDB(async (db) => {
+//             var objectId = ObjectID.createFromHexString(id);
+//             await db.collection('pieces').updateOne({ _id: objectId }, {
+//                 '$set': {
+//                     titre: titre,
+//                     artiste: artiste,
+//                     categorie: categorie
+//                 }
+//             });
             
-            reponse.status(200).send("Pièce modifiée");
-        }, reponse).catch(
-            () => reponse.status(500).send("Erreur : la pièce n'a pas été modifiée")
-        );        
-    }
-    else {
-        reponse.status(500).send(`Certains paramètres ne sont pas définis :
-            - titre: ${titre}
-            - artiste: ${artiste}
-            - categorie: ${categorie}`);
-    }
-});
+//             reponse.status(200).send("Pièce modifiée");
+//         }, reponse).catch(
+//             () => reponse.status(500).send("Erreur : la pièce n'a pas été modifiée")
+//         );        
+//     }
+//     else {
+//         reponse.status(500).send(`Certains paramètres ne sont pas définis :
+//             - titre: ${titre}
+//             - artiste: ${artiste}
+//             - categorie: ${categorie}`);
+//     }
+// });
 
-app.delete('/api/pieces/supprimer/:id', (requete, reponse) => {
-    const id = requete.params.id;
+// app.delete('/api/pieces/supprimer/:id', (requete, reponse) => {
+//     const id = requete.params.id;
 
-    utiliserDB(async (db) => {
-        var objectId = ObjectID.createFromHexString(id);
-        const resultat = await db.collection('pieces').deleteOne({ _id: objectId });
+//     utiliserDB(async (db) => {
+//         var objectId = ObjectID.createFromHexString(id);
+//         const resultat = await db.collection('pieces').deleteOne({ _id: objectId });
         
-        reponse.status(200).send(`${resultat.deletedCount} pièce supprimée`);
-    }, reponse).catch(
-        () => reponse.status(500).send("Erreur : la pièce n'a pas été supprimée")
-    );    
-});
+//         reponse.status(200).send(`${resultat.deletedCount} pièce supprimée`);
+//     }, reponse).catch(
+//         () => reponse.status(500).send("Erreur : la pièce n'a pas été supprimée")
+//     );    
+// });
